d4f5085389b140b284bfc2fb8f1a5e1687869d8a
Yashikaa20 <yashikaa204@gmail.com>
2024-07-23 16:54:55 +0530

    Add files via upload

---
+++ prgrm1.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+using namespace std;
+int main(){
+  int i=1;
+  int n;
+  cin>>n;
+  while(i<=n){
+    int j=1;
+    while(j<=n){
+      cout<<n-j+1;
+      j++;
+    }
+    i++;
+    cout<<endl;
+  }
+}
